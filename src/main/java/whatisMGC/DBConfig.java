3eff18ee23342c04776957e8103d7f4359b359a2
iubns <iubns@naver.com>
2025-09-15 13:57:12 +0900

    fix: DB_USERNAME

--- src/main/java/whatisMGC/DBConfig.java
+++ src/main/java/whatisMGC/DBConfig.java
@@ -8,7 +8,7 @@ public class DBConfig {
     public static final String DB_HOST = dotenv.get("DB_HOST");
     public static final String DB_PORT = dotenv.get("DB_PORT");
     public static final String DB_NAME = dotenv.get("DB_NAME");
-    public static final String DB_USER = dotenv.get("DB_USER");
+    public static final String DB_USER = dotenv.get("DB_USERNAME");
     public static final String DB_PASSWORD = dotenv.get("DB_PASSWORD"); //
 
     public static final String DB_URL = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME + "?useSSL=false&serverTimezone=UTC";
