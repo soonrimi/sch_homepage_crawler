57c6dd5521d344daf5ddeed4bf4c798a9b5805d9
conconcc <lesj9217@gmail.com>
2025-10-31 14:25:48 +0900

    refactor : 인간이 아닌 작동으로 많이 작동하고 있어 인간처럼 작동할 수 있도록 수정

--- src/main/java/whatisMGC/HtmlFetcher.java
+++ src/main/java/whatisMGC/HtmlFetcher.java
@@ -10,10 +10,17 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class HtmlFetcher {
-//url을 받아 접속, 해당 url의 html 문서 가져오기
+    //웹브라우저의 USER-AGENT 설정
+    private static final String CHROME_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36";
+    //url을 받아 접속, 해당 url의 html 문서 가져오기
     public Document getHTMLDocument(String url) {
         try{
-            Connection.Response response =  Jsoup.connect(url).timeout(30 * 1000).execute();
+            Connection.Response response =  Jsoup.connect(url)
+                    .userAgent(CHROME_USER_AGENT) //사람처럼 행동하는 브라우저로 가장
+                    .header("Accept-Language", "ko-KR,ko;q=0.9") // 한국어 페이지 우선 요청
+                    .timeout(30 * 1000) //30초 타임아웃
+                    .followRedirects(true) //리다이렉트 시 리다이렉트 허용
+                    .execute();
             return Jsoup.parse(response.body(), url);
 
         } catch (IOException e) {
@@ -22,18 +29,18 @@ public class HtmlFetcher {
         }
     }
     //
-public String extractBaseURLFromURL(String url) throws URISyntaxException {
-    URI uri = new URI(url);
-    String path = uri.getPath();
-    int secondSlashIndex = path.indexOf('?', 0);
-    String trimmedPath=path;
-    if (secondSlashIndex > 0) {
-        trimmedPath = path.substring(0, secondSlashIndex);
+    public String extractBaseURLFromURL(String url) throws URISyntaxException {
+        URI uri = new URI(url);
+        String path = uri.getPath();
+        int secondSlashIndex = path.indexOf('?', 0);
+        String trimmedPath=path;
+        if (secondSlashIndex > 0) {
+            trimmedPath = path.substring(0, secondSlashIndex);
+        }
+        return uri.getScheme() + "://" + uri.getAuthority()+trimmedPath;
     }
-    return uri.getScheme() + "://" + uri.getAuthority()+trimmedPath;
-}
 
-    }
+}
 
 
 
